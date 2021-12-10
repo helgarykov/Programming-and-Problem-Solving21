6ed6e26068ea2cf697e6500d31e01118606ecc58
Helga Rykov Ibsen <helgarykovibsen@gmail.com>
2021-12-10 17:14:55 +0100

    add changes

--- Uge10/simulate.fs
+++ Uge10/simulate.fs
@@ -8,32 +8,48 @@ module SimulateDrones
 ///<param name="f">A .</param>
 ///<returns> Reached destination or not as a bool.</returns> 
 
-type Drone (x:int,y:int, x1:int, y1:int, s:int) =
-    let mutable x = x
-    let mutable x1 = x1
-    let mutable y = y
-    let mutable y1 = y1
+type Drone (x:int, y:int, x1:int, y1:int, s:int) =
+    //let mutable x = x
+    //let mutable x1 = x1
+    //let mutable y = y
+    //let mutable y1 = y1
     let mutable position = (x, y)
+    let mutable destination = (x1, y1)
     let mutable speed = s 
-    member this.Destination = (x1, y1)
-    //member this.Fly = x <- (sin (x - x1 +s)) ; y <- (sin (y - y1, s)) // FORKERT
 
-
-    member this.Fly = 
-        if FrameCount % 60 = 0 then
-            position <- position + 2 * speed
-
-    member this.AtDestination = 
-        //match this.AtDestination with
-        //| position -> true
-        //|      
-    if position = this.Destination then true else false
+    member this.fly = 
+        let direction = ((x1 - x), (y1 - y))
+        let scala = ((s * (x1 - x)), (s * (y1 - y))) 
+        
 
 
 
-type Airspace (drons:drone) =
+        //let directionNorm = ((s * (x1 - x)), (s * (y1 - y))) 
+        let distance = sqrt (((x1 - x) ** 2) + ((y1 - y) ** 2))
+
+
+        let distanceNorm = s * distance
+        let newPosition = position + distanceNorm 
+        newPosition
+        
+    member this.destination =
+        if destination = position then
+            position
+        else
+            destination
+
+
+    member this.atDestination =    
+        if position = this.destination then 
+            true 
+        else 
+            false
+
+let drone1 = Drone (10, 10, 50, 50, 5)
+
+(* type Airspace (drons:drone) =
     let mutable drones = 
     member this.DroneDist =
     member this.FlyDrones =
     member this.AddDrone =
-    member this.WillCollide =
+    member this.WillCollide = *)
