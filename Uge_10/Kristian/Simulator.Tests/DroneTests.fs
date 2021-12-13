49c87c5b9b99a59d9260c8299b1ab17f35c7e972
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2021-12-13 01:01:15 +0100

    Kristians partial solution

---
+++ Uge_10/Kristian/Simulator.Tests/DroneTests.fs
@@ -0,0 +1,60 @@
+﻿namespace HRI.Drones
+
+open System
+open Xunit
+
+open Primitive
+open Simulation
+
+module DroneTests =
+
+    [<Fact>]
+    let ``Can construct a drone`` () =
+        let d = Drone(Vector(1.0, 2.0), Vector(4.0, 4.0), 1.0)
+
+        Assert.Equal(Vector(1.0, 2.0), d.Position)
+        Assert.Equal(Vector(4.0, 4.0), d.Destination)
+        Assert.Equal(1.0, d.Speed)
+
+    [<Fact>]
+    let ``A drone can fly`` () = 
+        let initP = Vector(1.0, 2.0)
+        let d = Drone(initP, Vector(4.0, 4.0), 1.0)
+
+        d.Fly()
+
+        Assert.NotEqual(d.Position, initP)
+        Assert.Equal(1.8321, d.Position.X, 4)
+        Assert.Equal(2.5547, d.Position.Y, 4)
+
+        Assert.False(d.AtDestination())
+
+    [<Fact>]
+    let ``A drone will stop at its destination`` () = 
+        let initP = Vector(1.0, 2.0)
+        let d = Drone(initP, Vector(4.0, 4.0), 1.0)
+
+        d.Fly()
+        d.Fly()
+        d.Fly()
+        d.Fly()
+
+        Assert.NotEqual(d.Position, initP)
+        Assert.Equal(4.0, d.Position.X, 4)
+        Assert.Equal(4.0, d.Position.Y, 4)
+
+        Assert.True(d.AtDestination())
+
+    [<Fact>]
+    let ``We can construct a list of all pairs of drones`` () =
+        let a = Drone(Vector(0.0, 0.0), Vector(100.0, 0.0), 1.0)
+        let b = Drone(Vector(20.0, 0.0), Vector(100.0, 0.0), 1.0)
+        let c = Drone(Vector(-50.0, -50.0), Vector(25.0, 50.0), 1.0)
+        let d = Drone(Vector(-200.0, 15.0), Vector(-75.0, 100.0), 1.0)
+        let e = Drone(Vector(350.0, 855.0), Vector(0.0, 0.0), 1.0)
+
+        let l = [a; b; c; d; e]
+
+        let p = pairs l
+
+        Assert.Equal(10, p.Length)
---
+++ Uge_10/Kristian/Simulator/Simulator.fs
@@ -0,0 +1,90 @@
+﻿namespace HRI.Drones
+
+module Primitive =
+
+    type Vector(x: float, y: float) =
+        member this.X = x
+        member this.Y = y
+
+        member this.Length =
+            sqrt (this.X ** 2.0 + this.Y ** 2.0)
+
+        member this.Normalized() =
+            this / this.Length
+
+        override this.GetHashCode() =
+            hash (this.X, this.Y)
+
+        override this.Equals(b) =
+            match b with
+            | :? Vector as v -> this.X = v.X && this.Y = v.Y
+            | _ -> false
+
+        static member (+) (l: Vector, r: Vector): Vector =
+            Vector(l.X + r.X, l.Y + r.Y)
+
+        static member (-) (l: Vector, r: Vector): Vector =
+            Vector(l.X - r.X, l.Y - r.Y)
+
+        static member (*) (l: Vector, m: float): Vector =
+            Vector(l.X * m, l.Y * m)
+
+        static member (/) (l: Vector, r: float): Vector =
+            Vector(l.X / r, l.Y / r)
+
+module Simulation =
+
+    type Drone(position: Primitive.Vector, destination: Primitive.Vector, speed: float) =
+        let mutable position = position
+        let mutable destination = destination
+        let mutable speed = speed
+        
+        member this.Position = position
+        member this.Destination = destination
+        member this.Speed = speed
+         
+        member this.Fly() =
+            let toTarget = destination - position
+
+            position <- 
+                match toTarget.Length with
+                | d when d <= speed -> destination
+                | _ -> position + (toTarget.Normalized() * speed)
+
+        member this.AtDestination() =
+            position = destination
+
+        override this.ToString() =
+            sprintf "X = %f, Y = %f, Speed = %f" position.X position.Y speed
+
+    let pairs (l: 'a list) =
+        let rec p (i: 'a) (l: 'a list) (acc: ('a * 'a) list) =
+            match l with
+            | [] -> acc
+            | head :: tail -> p i tail ((i, head) :: acc)
+
+        let rec r (l: 'a list) (acc: ('a * 'a) list) =
+            match l with
+            | [] -> acc
+            | head :: tail -> r tail (p head tail acc)
+
+        let e: ('a * 'a) list = []
+        r l e
+
+    type Airspace(d: Drone list) = 
+        let mutable drones: Drone list = d
+
+        member this.DroneDist (a: Drone) (b: Drone) =
+            (a.Position - b.Position).Length
+
+        member this.AllWithinRange() = 
+            drones |> pairs |> List.filter (fun (x, y) -> this.DroneDist x y < 5.0)
+
+        member this.AddDrone(d) =
+            drones <- d :: drones
+
+        member this.FlyDrones() =
+            drones |> List.iter (fun x -> x.Fly())
+
+        member this.WillCollide() =
+            let x = 5
